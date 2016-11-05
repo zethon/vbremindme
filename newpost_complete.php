8df0b8b7f5ee0f68286fc40be90659abec739ce0
Addy <aclaure@gmail.com>
2016-11-05 09:22:41 -0400

    looking better;

--- newpost_complete.php
+++ newpost_complete.php
@@ -1,55 +1,59 @@
 <?
 
-global $vbulletin;
+global $vbulletin,$db;
 $vbo = &$vbulletin->options;
 $vbu = &$vbulletin->userinfo;
 
 $botuserid = 2976;
 
-if ($vbu['userid'] == 1)
+if (isset($vbo["remindme_dateline"]))
 {
-    if (isset($vbo["remindme_dateline"]))
+    // create the reminder
+    $newposturl = $vbo['bburl'] . "/showthread.php?p=" . $post['postid'] . "#post". $post['postid'];
+    $remindertext = "I am the RemindMe Bot reminding you about this post: [url]$newposturl[/url]";
+    if (isset($vbo["remindme_msgtext"]) && strlen($vbo["remindme_msgtext"]) > 0)
     {
-        // TODO: create the reminder
-
-        // create a new post informing the user we've created the reminder
-        $datetxt = vbdate($vbo['dateformat'], $vbo["remindme_dateline"]);
-        $timetxt = vbdate($vbo['timeformat'], $vbo["remindme_dateline"]);
-
-        $quotetext = "[QUOTE=$post[username];$post[postid]]$post[message][/QUOTE]";
-        $msgtext = "$quotetext\n\nI'll message you on $datetxt $timetxt to remind you of this post";
-        if (isset($vbo["remindme_msgtext"]) && strlen($vbo["remindme_msgtext"]) > 0)
-        {
-            $msgtext = "$quotetext\n\nI'll message you on $datetxt at $timetxt to remind you of this post with the message[QUOTE]$vbo[remindme_msgtext][/QUOTE]";
-        }
-
-        $newpost =& datamanager_init('Post', $vbulletin, $error_type, 'threadpost');
-        $newpost->set('userid', $botuserid); 
-        $newpost->set_info('forum', $foruminfo);
-        $newpost->set_info('thread', $threadinfo); 
-        $newpost->set_info('is_automated', true);
-        $newpost->set_info('skip_floodcheck', true);        
-        $newpost->set('threadid', $threadinfo['threadid']);
-        $newpost->set('parentid', $post['postid']);
-        $newpost->set('pagetext', $msgtext); 
-        $newpost->set('visible', true);
-        $newpost->set('allowsmilie',  true);
-        $newpost->set('showsignature',true);
-        $newpost->pre_save();
-        if(count($newpost->errors) < 1)
-        {
-            $newid = $newpost->save();
-            unset($newpost);
-            build_thread_counters($newpost);
-        }
-        else
-        {
-            print "<h1>Error making new post! " . $newpost->errors[0] . $newpost->errors[1] . $newpost->errors[2] ."</h1>";   
-        }   
-
-        // just to be sure
-        unset($vbo["remindme_dateline"]);
+        $remindertext .= "\n\n[QUOTE]$vbo[remindme_msgtext][/QUOTE]"; 
     }
+    $db->query_write("INSERT INTO ".TABLE_PREFIX."remindme (userid, dateline, message) VALUES ($vbu[userid],$vbo[remindme_dateline],'$remindertext');");
+
+    // create a new post informing the user we've created the reminder
+    $datetxt = vbdate($vbo['dateformat'], $vbo["remindme_dateline"]);
+    $timetxt = vbdate($vbo['timeformat'], $vbo["remindme_dateline"]);
+
+    $quotetext = "[QUOTE=$post[username];$post[postid]]$post[message][/QUOTE]";
+    $botposttext = "$quotetext\n\nI will send you a private message on $datetxt $timetxt to remind you of this post";
+    if (isset($vbo["remindme_msgtext"]) && strlen($vbo["remindme_msgtext"]) > 0)
+    {
+        $botposttext = "$quotetext\n\nI will send you a private message on $datetxt at $timetxt to remind you of this post with the message[QUOTE]$vbo[remindme_msgtext][/QUOTE]";
+    }
+
+    $newpost =& datamanager_init('Post', $vbulletin, $error_type, 'threadpost');
+    $newpost->set('userid', $botuserid); 
+    $newpost->set_info('forum', $foruminfo);
+    $newpost->set_info('thread', $threadinfo); 
+    $newpost->set_info('is_automated', true);
+    $newpost->set_info('skip_floodcheck', true);        
+    $newpost->set('threadid', $threadinfo['threadid']);
+    $newpost->set('parentid', $post['postid']);
+    $newpost->set('pagetext', $botposttext); 
+    $newpost->set('visible', true);
+    $newpost->set('allowsmilie',  true);
+    $newpost->set('showsignature',true);
+    $newpost->pre_save();
+    if(count($newpost->errors) < 1)
+    {
+        $newid = $newpost->save();
+        unset($newpost);
+        build_thread_counters($newpost);
+    }
+    else
+    {
+        print "<h1>Error making new post! " . $newpost->errors[0] . $newpost->errors[1] . $newpost->errors[2] ."</h1>";   
+    }   
+
+    // just to be sure
+    unset($vbo["remindme_dateline"]);
 }
 
 ?>
--- remindmecron.php
+++ remindmecron.php
@@ -5,55 +5,38 @@ $DEBUG = true;
 
 $botuserid = 2976;
 
-// this means a server cron job is launching this or we are 
-// manually launching it from a shell
-if ($isconsole) 
-{
-	// allows us to include vbulletin framework in a console script
-	define('NO_REGISTER_GLOBALS', 1);
-	define('SKIP_SESSIONCREATE', 1);
-	define('SESSION_BYPASS', 1);
-	define('NOCOOKIES', 1);
-	//define('DIE_QUIETLY', 1);
-	//error_reporting(0);	
-	chdir('..');
-//require_once('./install/init.php');
-//require_once('./../includes/functions.php');
-//require_once(DIR . '/install/upgrade_language_en.php');
-
-	print("xxxxxxx\n");
-	print (">" . getcwd()); print("]\n");	
-
-print("1111111111111\n");
-	require_once('/amb/www/global.php'); 
-	print("222222222222\n"); 
-//	$db = $vbulletin->db;
-	print("\nBYE!!!");
-
-}
-else // this means vbulletin's scheduled tasks is running us
-{
-	$db = $vbulletin->db;
-}
-
-print("-----------------------\n");
-
+$db = $vbulletin->db;
 $botinfo = fetch_userinfo($botuserid);
-print("<pre>"); print_r($botinfo); print("</pre>");
+$permissions = cache_permissions($botinfo);
 
 $reminders = $db->query_read("SELECT * FROM ". TABLE_PREFIX ."remindme");
 while ($reminderinfo = $db->fetch_array($reminders))
 {
-	print_r($reminderinfo);
-	$pmdm =& datamanager_init('PM', $vbulletin, ERRTYPE_ARRAY);
-	$pmdm->set_info('savecopy', false);
-	$pmdm->set_info('cantrackpm', false);
-	$pmdm->set('fromuserid', $botuserid);
-	$pmdm->set('fromusername', $botinfo['username']);	
-	$pmdm->setr('title', 'RemindeMe Message');
+	if ($reminderinfo['dateline'] < TIMENOW)
+	{
+		print("<pre>"); print_r($reminderinfo); print("</pre>");
 
+		$touserinfo = fetch_userinfo($reminderinfo['userid']);
+
+		$pmdm =& datamanager_init('PM', $vbulletin, ERRTYPE_ARRAY);
+		$pmdm->set('dateline', TIMENOW);
+		$pmdm->set('fromuserid', $botinfo['userid']);
+		$pmdm->set('fromusername', $botinfo['username']);
+		$pmdm->set_recipients($touserinfo['username'], $permissions, 'cc');
+		$pmdm->set('title', 'RemindMe Bot Reminder');
+		$pmdm->set('message', $reminderinfo['message']);
+		$pmdm->set_info('savecopy', false);
+		$pmdm->set_info('cantrackpm', false);
+		$pmdm->set('showsignature', true);
+		$pmdm->set('allowsmilie', true);
+		
+		$pmdm->pre_save();
+
+		if(count($pmdm->errors) < 1)
+		{
+			$pmdm->save();
+		}
+	}
 }
-
-print("xxxxx");
 
 ?>
